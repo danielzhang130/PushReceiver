b110b546036d135625fb02b01641ab3e96092efd
danielzhang130 <danielzhang130@gmail.com>
2023-02-12 12:56:42 -0500

    update log tag

--- app/src/main/java/com/example/pushreceiver/MainActivity.kt
+++ app/src/main/java/com/example/pushreceiver/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.pushreceiver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
-import android.widget.TextView
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.messaging.FirebaseMessaging
-import com.google.android.gms.tasks.OnSuccessListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(imageView)
         FirebaseMessaging.getInstance().apply {
             token.addOnSuccessListener { s: String? ->
-                Log.d("", s.orEmpty())
+                Log.d(this@MainActivity.toString(), s.orEmpty())
                 val encoder = QRGEncoder(s, QRGContents.Type.TEXT, 500)
                 val bitmap = encoder.bitmap
                 imageView.setImageBitmap(bitmap)
